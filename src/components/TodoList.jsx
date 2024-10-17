a777f9740c6a14fc5d32808951972522bb0a0228
adilraees786 <adilraees911@gmail.com>
2024-10-17 19:32:04 +0500

    css

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,46 +1,46 @@
 import React from 'react'
 
-const TodoList = ({todos, setTodos, setEditTodo}) => {
+const TodoList = ({ todos, setTodos, setEditTodo }) => {
 
 
-  const handleComolete=({id}) => {
+  const handleComolete = ({ id }) => {
     setTodos(
-      todos.map((todo) => todo.id === id ? {...todo, completed: !todo.completed} : todo))
+      todos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
   }
 
-  const handleDelete=({id}) => {
+  const handleDelete = ({ id }) => {
     setTodos(todos.filter((todo) => todo.id !== id))
   }
 
-  const handleEdit = ({id}) => {
+  const handleEdit = ({ id }) => {
     const findTodo = todos.find((todo) => todo.id === id);
     setEditTodo(findTodo)
   }
   return (
     <div>
-        {todos.map((todo) =>(
-            <li className="todo-list" key={todo.id}>
-                <input type="text" 
-                value={todo.title} 
-                className={`list ${todo.completed ? "complete" : ""}`} 
-                onChange={(event) => event.preventDefault()}/>
-<div>
+      {todos.map((todo) => (
+        <li className="todo-list" key={todo.id}>
+          <input type="text"
+            value={todo.title}
+            className={`list ${todo.completed ? "complete" : ""}`}
+            onChange={(event) => event.preventDefault()} />
 
-  <button className='button-complete task-button' onClick={()=> handleComolete(todo)}>
-    <i className='fa fa-check-circle'></i>
-  </button>
-  <button className='button-edit task-button' onClick={()=> handleEdit(todo)}>
-    <i className='fa fa-edit'></i>
-  </button>
-  <button className='button-delete task-button' onClick={()=> handleDelete(todo)}>
-    <i className='fa fa-trash'></i>
-  </button>
-</div>
+          <div className='btns'>
+            <button className='button-complete task-button' onClick={() => handleComolete(todo)}>
+              <i className='fa fa-check-circle'></i>
+            </button>
+            <button className='button-edit task-button' onClick={() => handleEdit(todo)}>
+              <i className='fa fa-edit'></i>
+            </button>
+            <button className='button-delete task-button' onClick={() => handleDelete(todo)}>
+              <i className='fa fa-trash'></i>
+            </button>
+          </div>
 
 
 
-            </li>
-        ))}
+        </li>
+      ))}
     </div>
   );
 };
